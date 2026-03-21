0e7aff5780fe322a69554414e6337d8a39a386c2
EmadRefaat <emad.refaat57@gmail.com>
2026-03-21 23:52:00 +0200

    Build a layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,72 @@
-import { Button, HStack } from "@chakra-ui/react";
+import { Box, Grid, GridItem } from "@chakra-ui/react";
+
 function App() {
   return (
-    <>
-      <HStack>
-        <Button margin={2} backgroundColor={"red"}>
-          Click me
-        </Button>
-        <Button>Click me</Button>
-      </HStack>
-    </>
+    <Grid
+      templateColumns={{
+        base: "1fr",
+        lg: "repeat(12, 1fr)",
+      }}
+      templateRows={{
+        base: "auto 1fr",
+        lg: "auto 1fr",
+      }}
+      gap={1}
+      minHeight="100vh"
+    >
+      {/* Navbar - Full width */}
+      <GridItem
+        colSpan={{
+          base: 1,
+          lg: 12,
+        }}
+        bg="blue.500"
+        p={2}
+        height={10}
+      >
+        <Box color="white" fontWeight="bold">
+          Navbar
+        </Box>
+      </GridItem>
+
+      {/* Aside - 2 columns on large screens, hidden on mobile */}
+      <GridItem
+        colSpan={{
+          base: 1,
+          lg: 2,
+        }}
+        rowSpan={{
+          base: 1,
+          lg: 2,
+        }}
+        bg="gray.200"
+        p={4}
+        display={{
+          base: "none",
+          lg: "block",
+        }}
+        height="100%"
+      >
+        <Box fontWeight="bold">Aside</Box>
+      </GridItem>
+
+      {/* Main - 10 columns on large screens, full width on mobile */}
+      <GridItem
+        colSpan={{
+          base: 1,
+          lg: 10,
+        }}
+        rowSpan={{
+          base: 1,
+          lg: 2,
+        }}
+        bg="gray.100"
+        p={4}
+        height="100%"
+      >
+        <Box fontWeight="bold">Main Content</Box>
+      </GridItem>
+    </Grid>
   );
 }
 
