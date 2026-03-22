73e1af1cd59f35d73eff345f9a0b88c279884841
EmadRefaat <emad.refaat57@gmail.com>
2026-03-22 13:44:09 +0200

    Appling dark mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,14 @@
 import { Box, HStack, Text, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import { ColorModeButton } from "./ui/color-mode";
 const Navbar = () => {
   return (
     <>
-      <HStack>
+      <HStack justifyContent="space-between" p={1}>
         <Box>
           <Image src={logo} w={"60px"} />
         </Box>
-        <Text>Navbar</Text>
+        <ColorModeButton></ColorModeButton>
       </HStack>
     </>
   );
